39e95854355da3ed8eb1805681edddf7266eaa97
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-10-23 18:24:37 +0700

    added Bootstrap 5.1 and some FA icons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-dark text-white">
       Things are working!
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './styles/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
