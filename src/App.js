a2882ae812997bef5c1d756c7050011c879ccbe4
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-10-23 18:34:59 +0700

    created CreditBox component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+/* eslint-disable */
+
 import './App.css';
+import { QuoteBox } from './components/QuoteBox.jsx';
+import { CreditBox } from './components/CreditBox.jsx';
 
 function App() {
   return (
-    <div className="bg-dark text-white">
-      Things are working!
+    <div className="container-fluid min-vh-100 min-vw-100 justify-content-center align-items-center">
+      <QuoteBox />
+      <CreditBox />
     </div>
   );
 }
---
+++ src/components/CreditBox.jsx
@@ -0,0 +1,8 @@
+export const CreditBox = () => (
+  <address className="author">
+    Created with
+    <span className="mx-2 fs-5">♥</span>
+    by
+    <a href="https://github.com/harmonify" className="ms-2 text-decoration-underline" rel="author">Wendy</a>
+  </address>
+);
