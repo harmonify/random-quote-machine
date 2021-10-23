e38ceae72c3ec90c0b1ae812c2a07f0e12dc2964
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-10-23 21:38:40 +0700

    update CreditBox component

--- src/components/CreditBox.jsx
+++ src/components/CreditBox.jsx
@@ -1,8 +1,8 @@
 export const CreditBox = () => (
-  <address className="author">
+  <address className="my-2 fs-6 text-white">
     Created with
-    <span className="mx-2 fs-5">♥</span>
+    <i className="bi bi-heart-fill mx-2 text-danger"></i>
     by
-    <a href="https://github.com/harmonify" className="ms-2 text-decoration-underline" rel="author">Wendy</a>
+    <a href="https://github.com/harmonify" className="ms-2 text-reset text-decoration-underline" rel="author">Wendy</a>
   </address>
 );
