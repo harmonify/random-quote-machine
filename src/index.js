e5c1be92bc69f1c745b6a54af8af1c67e0b766df
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-10-24 13:51:14 +0700

    use CDN for styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap-icons/font/bootstrap-icons.css'
 import './styles/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
